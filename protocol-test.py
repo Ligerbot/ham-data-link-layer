b9a25b1f1b0dcdc363234fef6fdaf7d46f6367f1
Ligerbot <ilan@snackmail.cc>
2026-06-02 08:41:50 -0700

    Almost completely reassambles strings sent that take up more than one packet

--- protocol-test.py
+++ protocol-test.py
@@ -5,30 +5,68 @@ import subprocess
 rsc = reedsolo.RSCodec(10)
 #file = True
 def recieve():
+	global newframes
+	newframes = []
+	breaknow = False
+	proc = subprocess.Popen(
+		['minimodem', '--rx', '1200', '--confidence', '0.1', '-q'],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.DEVNULL
+	)
 	while True:
-		proc = subprocess.Popen(
-			['minimodem', '--rx', '300', '--confidence', '0.1', '-q'],
-			stdout=subprocess.PIPE,
-			stderr=subprocess.DEVNULL
-		)
 		recieved = proc.stdout.readline()
-		proc.terminate()
-		proc.terminate()
-		proc.terminate()
-		proc.terminate()
-		print(recieved)
+#		print(recieved)
 		frame = unframe(recieved)
+		if frame != None:
+			newframes.append(frame)
+#		if breaknow == True:
+#			print("Newframes: " + str(newframes))
+#			break
+	print(newframes)
+	proc.terminate()
+	proc.terminate()
+	proc.terminate()
+	proc.terminate()
 
 def unframe(raw):
+	global breaknow
+	global newframes
 	splitted = raw.split(b"{|{|{|")
-
-	print("Splitted: " + str(splitted))
+#	print("Splitted: " + str(splitted))
+#	breaknow = False
 	for item in splitted:
 		if b"|}|}|}" in item or b"|||" in item:
 			print("Sending ackgnowledgement")
+#			newframes.append(item)
 			tones.play(1000, 500)
-		else:
-			print("Not a packet")
+			return rsc.decode(b"{|{|{|" + item)[0]
+#			return b"{|{|{|" + item
+		elif b"{OK}" in item:
+			breaknow = True
+			tones.play(1000, 500)
+#			exit(1)
+			print("Exiting from recieve loop, recieving cconnection close")
+			content = ""
+			for frame in newframes:
+				frame = frame.replace(b"{|{|{|", b"")
+				frame = frame.replace(b"|}|}|}", b"")
+				text = frame.split(b"|||")[1]
+				length = frame.split(b"|||")[0]
+				print("Frame length: " + str(length))
+				print("Frame content: " + str(text))
+#				text = str(text).replace("bytearray'", "")
+				text = text.decode("ascii", errors="ignore")
+				if int(length) <= 10:
+					content = content + str(text)
+				else:
+					while len(str(text)) >= 11:
+						text = str(text)[:-1] #claude says this deletes last char
+					content = content + str(text)
+#					print("Length error")
+			print("Reassembled string: " + content)
+			print(newframes)
+#		else:
+#			print("Not a packet")
 #		sine(500, 700) #ackgnowledge ment tone
 #	intermediate = raw.replace(b"{|{|{|", b"")
 #	intermediate = intermediate.replace(b"|}|}|}", b"")
@@ -104,7 +142,7 @@ def format(message):
 def send(message):
 	print("Sending: " + str(message))
 	proc = subprocess.Popen(
-		['minimodem', '--tx', '300', '--confidence', '0.3'],
+		['minimodem', '--tx', '1200', '--confidence', '0.3'],
 		stdin=subprocess.PIPE,
 		stderr=subprocess.DEVNULL
 	)
@@ -122,15 +160,21 @@ if role == "1":
 	#raw = b""
 #	send(b"\n") #start of new frame
 	for frame in frames:
-		send(b"\n" + bytes(frame) + b"\n")
-		time.sleep(0.5)
+#		send(b"\n" + bytes(frame) + b"\n")
+#		time.sleep(0.5)
 		while True:
-			if tones.listen(1000):
+			if tones.listen(1000, duration_ms=2000):
 				print("Got ackgnowledgement, continuing to next packet")
 				break
 			print("Didn't recieve ackgnowledgement. Resending the frame")
-			time.sleep(1)
+#			time.sleep(1)
 			send(b"\n" + bytes(frame) + b"\n")
+	while True:
+		if tones.listen(1000, duration_ms=2000):
+			print("Done")
+			break
+		print("Never recieved ack")
+		send(b"\n{OK}\n{OK}\n")
 #			time.sleep(0.5)
 #		time.sleep(0.5)
 	#	raw = raw + bytes(frame)
