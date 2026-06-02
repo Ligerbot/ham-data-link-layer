055758aa21c867fef42d423b795b2e61142d79da
Ligerbot <ilan@snackmail.cc>
2026-06-02 05:32:14 -0700

    Copied work over to repository

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ham-data-link-layer
+# Ham Radio Data Link Layer
+
+The goal of this project to to make my own data link layer protocol for ham radio. I've tried to use AX.25, but it is seemingly impossible to decode it using AFSK in python.
---
+++ protocol-test.py
@@ -0,0 +1,121 @@
+import time
+import reedsolo
+import subprocess
+rsc = reedsolo.RSCodec(10)
+def recieve():
+	while True:
+		proc = subprocess.Popen(
+			['minimodem', '--rx', '300', '--confidence', '1.2', '-q'],
+			stdout=subprocess.PIPE,
+			stderr=subprocess.DEVNULL
+		)
+		recieved = proc.stdout.readline()
+		proc.terminate()
+		proc.terminate()
+		proc.terminate()
+		proc.terminate()
+		print(recieved)
+		frame = unframe(recieved)
+
+def unframe(raw):
+	splitted = raw.split(b"{|{|{|")
+	print("Splitted: " + str(splitted))
+#	intermediate = raw.replace(b"{|{|{|", b"")
+#	intermediate = intermediate.replace(b"|}|}|}", b"")
+#	
+#	raw.split(b"{|")
+#	parts = raw.split(b"{|{|{|")
+#	parts = parts[1:]  # weird sorcery with the [1:]
+#
+#	frames = []
+#	for part in parts:
+#		sides = part.split(b"|}|}|}")
+#		frame = sides[0]
+#		check = sides[1]
+#		frames.append(frame + check)
+#
+#	print(frames)
+#broken function to fix character deletions and insertions
+#still need to fix this to make the protocol more reliable
+#		print(raw)
+#	fixed = ""
+#	for i in range(len(frames)):
+#		frame = frames[i] + frames[i+1]
+#		if len(frame) + 12 < int(str(frames[3]) + str(frames[4])):
+#			while len(frame) + 12 < int(str(frames[3]) + str(frames[4])):
+#				frame = frame + "?"
+#		elif len(frame) + 12 > int(str(frames[3]) + str(frames[4])):
+#			while len(frame) + 12 > int(str(frames[3]) + str(frames[4])):
+#				frame = frame[:-1]
+#		fixed = fixed + rsc.decode(frame)[0]
+#	print(fixed)
+def chonk(message):
+	if len(message) <= 4:
+		print("Message does not need to be chunked")
+		oneitemarray = []
+		oneitemarray.append(message)
+		return oneitemarray
+	else:
+		chunks = []
+		i = 0
+		while i < len(message):
+			start = i
+			end = i + 10
+			chunk = message[start:end]
+			chunks.append(chunk)
+			i += 10
+#		print(chunks)
+		return chunks
+def format(message):
+	chunks = chonk(message)
+	print(chunks)
+	frames = []
+	for chunk in chunks:
+		thebytes = chunk.encode("ascii")
+#		parity = rsc.encode(thebytes)
+#		print("Parity: " + str(parity))
+
+#		frame = b"{|{|{|" + b"??" + b"|" + thebytes + b"|||" + parity + b"|}|}|}"
+#		if not(len(frame) <= 99 and len(frame) >=10):
+#			print("Frame length error")
+#			return
+#		frame = b"{|{|{|" + bytes(str(len(frame)), "ascii") + b"|" + thebytes + b"|||" + parity + b"|}|}|}"
+#		frames.append(frame)
+
+		template = b"{|{|{|" + b"??" + b"|||" + thebytes + b"|}|}|}"
+		if not(len(template) <= 99 and len(template) >=10):
+			print("Frame length error")
+			return
+		frame = b"{|{|{|" + bytes(str(len(template)), "ascii") + b"|||" + thebytes + b"|}|}|}"
+		frames.append(rsc.encode(frame))
+	print(frames)
+	return frames
+
+def send(message):
+	print("Sending: " + str(message))
+	proc = subprocess.Popen(
+		['minimodem', '--tx', '300', '--confidence', '0.5'],
+		stdin=subprocess.PIPE,
+		stderr=subprocess.DEVNULL
+	)
+	proc.stdin.write(message)
+	proc.stdin.close()
+	proc.wait()
+	proc.terminate()
+	proc.terminate()
+	proc.terminate()
+	proc.terminate()
+role = input("Sender(1) or reciever(2): ")
+if role == "1":
+	msg = input("Message: ")
+	frames = format(msg)
+	#raw = b""
+	for frame in frames:
+		send(bytes(frame))
+		time.sleep(0.5)
+	#	raw = raw + bytes(frame)
+	#unframe(raw)
+elif role == "2":
+	recieve()
+else:
+	print("Invalid option")
